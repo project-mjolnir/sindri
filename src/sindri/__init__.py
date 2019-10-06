791f561fa3b01acae534e1e42e625a0844c15d72
C.A.M. Gerlach <CAM.Gerlach@Gerlach.CAM>
2019-10-05 19:16:52 -0500

    Add basic Python project structure

---
+++ src/sindri/__init__.py
@@ -0,0 +1,5 @@
+"""
+Sindri: Receive, process, monitor, store and serve data from remote sensors.
+"""
+
+from sindri._version import __version__
---
+++ src/sindri/_version.py
@@ -0,0 +1,4 @@
+"""Version file."""
+
+VERSION_INFO = (0, 1, 0, "dev0")
+__version__ = '.'.join((str(version) for version in VERSION_INFO))
